7ae562d442ba7c2e67cc2e8f37533e376b4034be
Adrián <acasram665l@g.educaand.es>
2026-01-28 12:40:15 +0100

    i

--- src/main/java/org/pspro_activ2_castellano_ramos_adrian/chat/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.pspro_activ2_castellano_ramos_adrian.chat;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
